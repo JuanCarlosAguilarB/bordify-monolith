51ef17b3dade0625e6d8859250bf83baa9335f23
JuanC <abjuancarlos.12@gmail.com>
2024-06-13 23:31:11 -0500

    chore: create service for Board entity (create a valid entity).

---
+++ src/test/java/com/bordify/persistence/models/BoardModelTestService.java
@@ -0,0 +1,24 @@
+package com.bordify.persistence.models;
+
+import com.bordify.models.Board;
+
+import com.bordify.models.User;
+
+import java.util.UUID;
+
+public class BoardModelTestService  {
+
+    public static Board createValidBoard() {
+
+        User user = UserModelTestService.createValidUser();
+        UUID userId = user.getId();
+
+        return Board.builder()
+                .id(UUID.randomUUID())
+                .name("Test Board")
+                .user(user)
+                .userId(userId)
+                .build();
+    }
+
+}

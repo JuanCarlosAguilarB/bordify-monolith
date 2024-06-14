a2c0522c550756d980d805f33636daadd0dbbb5a
JuanC <abjuancarlos.12@gmail.com>
2024-06-13 22:40:40 -0500

    feat: create test for UserRepository: UserRepositoryShould.

--- src/test/java/com/bordify/persistence/UserRepositoryShould.java
+++ src/test/java/com/bordify/persistence/UserRepositoryShould.java
@@ -3,6 +3,7 @@ import com.bordify.models.User;
 import com.bordify.repositories.UserRepository;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -15,21 +16,51 @@ public class UserRepositoryShould {
     @Autowired
     private UserRepository userRepository;
 
-
+    @DisplayName("Should find user by email")
     @Test
     public void shouldFindUserByEmail() {
-
-        // Arrange
         User userTest = createValidEntity();
         userRepository.save(userTest);
 
-        // Act
         boolean hasUser = userRepository.existsByEmail(userTest.getEmail());
-
-        // Assert
 
         Assertions.assertTrue(hasUser);
     }
+
+    @DisplayName("Should find user by userName")
+    @Test
+    public void shouldFindUserByUsername() {
+        User userTest = createValidEntity();
+        userRepository.save(userTest);
+
+        boolean hasUser = userRepository.existsByUsername(userTest.getUsername());
+
+        Assertions.assertTrue(hasUser);
+    }
+
+    @DisplayName("Should get an user by userName")
+    @Test
+    public void shouldGetAnUserByUsername() {
+        User userTest = createValidEntity();
+        userRepository.save(userTest);
+
+        User user = userRepository.findByUsername(userTest.getUsername());
+
+        Assertions.assertEquals(userTest, user);
+    }
+
+
+    @DisplayName("Should get an user by email")
+    @Test
+    public void shouldGetAnUserByEmail() {
+        User userTest = createValidEntity();
+        userRepository.save(userTest);
+
+        User user = userRepository.findByEmail(userTest.getEmail());
+
+        Assertions.assertEquals(userTest, user);
+    }
+
 
     public User createValidEntity() {
         return User.builder()

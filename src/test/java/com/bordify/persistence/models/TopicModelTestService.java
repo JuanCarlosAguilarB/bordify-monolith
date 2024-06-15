81fd1de8dd8eccead37c63437d5dd5f427076ceb
JuanC <abjuancarlos.12@gmail.com>
2024-06-14 23:57:15 -0500

    chore: create service for Topic entity (create a valid entity).

---
+++ src/test/java/com/bordify/persistence/models/TopicModelTestService.java
@@ -0,0 +1,29 @@
+package com.bordify.persistence.models;
+
+import com.bordify.models.Board;
+import com.bordify.models.Color;
+import com.bordify.models.Topic;
+
+import java.util.UUID;
+
+public class TopicModelTestService {
+
+    public static Topic createValidTopic() {
+
+        Color color = ColorModelTestService.createValidColor();
+        Board  board = BoardModelTestService.createValidBoard();
+
+
+        Topic topic = Topic.builder()
+                .id(UUID.randomUUID())
+                .name("Test Topic")
+                .color(color)
+                .colorId(color.getId())
+                .board(board)
+                .boardId(board.getId())
+                .build();
+
+        return topic;
+    }
+
+}

238989643658d6e2204fd681e62dc41ee82c1b7c
JuanC <abjuancarlos.12@gmail.com>
2024-06-16 00:44:59 -0500

    chore: create test for user services.

---
+++ src/test/java/com/bordify/service/UserServiceShould.java
@@ -0,0 +1,73 @@
+package com.bordify.service;
+
+import com.bordify.exceptions.UserNotFoundException;
+import com.bordify.models.User;
+import com.bordify.persistence.models.UserModelTestService;
+import com.bordify.repositories.UserRepository;
+import com.bordify.services.JwtService;
+import com.bordify.services.UserService;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class UserServiceShould {
+
+    @InjectMocks
+    private UserService userService;
+
+    @Mock
+    private UserRepository userRepositoryMock;
+
+    @Mock
+    private JwtService jwtServiceMock;
+
+    @BeforeEach
+    public void setUp() {
+        Mockito.reset(userRepositoryMock, jwtServiceMock);
+    }
+
+    @Test
+    public void shouldFindUserByUsername() {
+        // Given
+        String userName = "XXXX";
+        User userTest = UserModelTestService.createValidUser();
+        userTest.setUsername(userName);
+
+        // When
+        when(userRepositoryMock.findByUsername(userName)).thenReturn(userTest);
+        User user = userService.getUserByUsername(userName);
+
+        // Then
+        Mockito.verify(userRepositoryMock, Mockito.times(1)).findByUsername(userName);
+        assertEquals(userName, user.getUsername());
+        assertEquals(userTest.getId(), user.getId());
+    }
+
+    @Test
+    public void shouldThrowUserNotFoundExceptionWhenUserNotFound() {
+        // Given
+        String userName = "XXXX";
+        when(userRepositoryMock.findByUsername(userName)).thenReturn(null);
+
+        // When/Then
+        UserNotFoundException exception = assertThrows(UserNotFoundException.class, () -> {
+            userService.getUserByUsername(userName);
+        });
+
+    }
+
+
+
+
+}

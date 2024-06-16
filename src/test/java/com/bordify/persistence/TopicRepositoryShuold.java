ab49e00e71443ea8dc48417d2f308f4fb2db523d
JuanC <abjuancarlos.12@gmail.com>
2024-06-15 21:52:33 -0500

    Fix: solved problem with entities not created in TopicRepositoryShuold.

--- src/test/java/com/bordify/persistence/TopicRepositoryShuold.java
+++ src/test/java/com/bordify/persistence/TopicRepositoryShuold.java
@@ -2,10 +2,17 @@ package com.bordify.persistence;
 
 import com.bordify.dtos.TopicListDTO;
 import com.bordify.models.Board;
+import com.bordify.models.Color;
 import com.bordify.models.Topic;
+import com.bordify.models.User;
 import com.bordify.persistence.models.BoardModelTestService;
+import com.bordify.persistence.models.ColorModelTestService;
 import com.bordify.persistence.models.TopicModelTestService;
+import com.bordify.persistence.models.UserModelTestService;
+import com.bordify.repositories.BoardRepository;
+import com.bordify.repositories.ColorRepository;
 import com.bordify.repositories.TopicRepository;
+import com.bordify.repositories.UserRepository;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -22,18 +29,33 @@ public class TopicRepositoryShuold {
     @Autowired
     private TopicRepository topicRepository;
 
+    @Autowired
+    private BoardRepository  boardRepository;
+
+    @Autowired
+    private ColorRepository  colorRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+
     @DisplayName("Should find one topic by boardId")
     @Test
     public void shouldFindOneTopicByBoardId() {
 
-        Topic topicTest = TopicModelTestService.createValidTopic();
-        Topic noRalatedTopicTest = TopicModelTestService.createValidTopic();
+        User  userTest = UserModelTestService.createValidUser();
+        userRepository.save(userTest);
 
-        List<Topic> topicList = new ArrayList<>();
-        topicList.add(topicTest);
-        topicList.add(noRalatedTopicTest);
+        Board  boardTest = BoardModelTestService.createValidBoard(userTest);
+        Board  noRalatedBoardTest = BoardModelTestService.createValidBoard(userTest);
+        boardRepository.saveAll(List.of(boardTest,noRalatedBoardTest));
 
-        topicRepository.saveAll(topicList);
+        Color colorTest = ColorModelTestService.createValidColor();
+        colorRepository.save(colorTest);
+
+        Topic topicTest = TopicModelTestService.createValidTopic(colorTest, boardTest);
+        Topic noRalatedTopicTest = TopicModelTestService.createValidTopic(colorTest, noRalatedBoardTest);
+        topicRepository.saveAll(List.of(topicTest, noRalatedTopicTest));
 
         Pageable pageable = Pageable.unpaged();
         List<TopicListDTO> topicFromDb = topicRepository.findByBoardIdCustom(topicTest.getBoardId(), pageable);

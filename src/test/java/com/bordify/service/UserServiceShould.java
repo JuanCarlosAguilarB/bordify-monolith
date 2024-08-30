dc70e82522c711592bbf044618a0c3774c67544f
JuanC <abjuancarlos.12@gmail.com>
2024-08-30 00:32:20 -0500

    feat: Implemented base unit test class

--- src/test/java/com/bordify/service/UserServiceShould.java
+++ src/test/java/com/bordify/service/UserServiceShould.java
@@ -8,22 +8,20 @@ import com.bordify.repositories.UserRepository;
 import com.bordify.services.JwtService;
 import com.bordify.services.UserService;
 
+import com.bordify.shared.infrastucture.controlles.UnitTestBaseClass;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.when;
 
-@ExtendWith(MockitoExtension.class)
-public class UserServiceShould {
+
+public class UserServiceShould extends UnitTestBaseClass {
 
     @InjectMocks
     private UserService userService;

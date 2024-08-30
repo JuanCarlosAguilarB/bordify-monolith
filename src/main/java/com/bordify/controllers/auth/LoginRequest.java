266bd6d6569c5d4bf01f670c174ffa3e0528a7c7
JuanC <abjuancarlos.12@gmail.com>
2024-08-30 00:28:55 -0500

    chore: added Bulder of lombok

--- src/main/java/com/bordify/controllers/auth/LoginRequest.java
+++ src/main/java/com/bordify/controllers/auth/LoginRequest.java
@@ -1,8 +1,10 @@
 package com.bordify.controllers.auth;
 
+import lombok.Builder;
 import lombok.Getter;
 
 @Getter
+@Builder
 public class LoginRequest {
     private String username;
     private String password;

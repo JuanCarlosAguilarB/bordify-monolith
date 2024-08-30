2fba65a0511b6b5e447b9dc72b8295d2ed0a37c4
JuanC <abjuancarlos.12@gmail.com>
2024-08-30 00:29:41 -0500

    feat: User random now is created

--- src/test/java/com/bordify/persistence/models/UserModelTestService.java
+++ src/test/java/com/bordify/persistence/models/UserModelTestService.java
@@ -5,6 +5,8 @@ import com.bordify.models.User;
 import java.time.LocalTime;
 import java.util.UUID;
 
+import static com.bordify.shared.domain.FactoryValues.*;
+
 public class UserModelTestService {
 
     public static User  createValidUser() {
@@ -25,6 +27,23 @@ public class UserModelTestService {
                 .build();
 
         return user;
+    }
+
+    public static User createRandomUser() {
+        LocalTime currentTime = LocalTime.now();
+
+        return User.builder()
+                .id(UUID.randomUUID())
+                .username(generateRandomAlphanumeric(10))
+                .password(generateRandomAlphanumeric(10))
+                .email(generateRandomEmail())
+                .firstName(generateRandomString(5))
+                .lastName(generateRandomString(7))
+                .isVerified(false)
+                .phoneNumber(generateRandomPhoneNumber())
+                .created(currentTime)
+                .lastLogin(null)
+                .build();
     }
 
 

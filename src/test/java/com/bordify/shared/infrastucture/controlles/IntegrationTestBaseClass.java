a7f08e2d4196adeeb638c910e6ede71f4cf05bd5
JuanC <abjuancarlos.12@gmail.com>
2024-08-30 00:43:33 -0500

    feat(!Important): Now, the test run in random order.

--- src/test/java/com/bordify/shared/infrastucture/controlles/IntegrationTestBaseClass.java
+++ src/test/java/com/bordify/shared/infrastucture/controlles/IntegrationTestBaseClass.java
@@ -8,7 +8,9 @@ import com.bordify.models.User;
 import com.bordify.repositories.UserRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -26,11 +28,11 @@ import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-
 @SpringBootTest()
 @AutoConfigureMockMvc
 @Transactional
 @Tag("integration")
+@TestMethodOrder(MethodOrderer.Random.class)
 abstract public class IntegrationTestBaseClass {
 
 
--- src/test/java/com/bordify/shared/infrastucture/controlles/UnitTestBaseClass.java
+++ src/test/java/com/bordify/shared/infrastucture/controlles/UnitTestBaseClass.java
@@ -1,10 +1,13 @@
 package com.bordify.shared.infrastucture.controlles;
 
+import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 @Tag("unit")
 @ExtendWith(MockitoExtension.class)
+@TestMethodOrder(MethodOrderer.Random.class)
 public class UnitTestBaseClass {
 }

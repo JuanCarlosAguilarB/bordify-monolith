bf79ac9b5f4a20a1290e3ed24e114f3778e95191
JuanC <abjuancarlos.12@gmail.com>
2024-08-30 00:31:52 -0500

    feat: Created base unit test class

---
+++ src/test/java/com/bordify/shared/infrastucture/controlles/UnitTestBaseClass.java
@@ -0,0 +1,10 @@
+package com.bordify.shared.infrastucture.controlles;
+
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@Tag("unit")
+@ExtendWith(MockitoExtension.class)
+public class UnitTestBaseClass {
+}

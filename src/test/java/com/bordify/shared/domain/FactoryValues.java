cbb8c56cfd08a333f7f91f8ee9053db109928f84
JuanC <abjuancarlos.12@gmail.com>
2024-08-30 00:28:23 -0500

    feat: create class for create a random dummy values

---
+++ src/test/java/com/bordify/shared/domain/FactoryValues.java
@@ -0,0 +1,33 @@
+package com.bordify.shared.domain;
+
+public class FactoryValues {
+
+    public static String generateRandomAlphanumeric(int length) {
+        String characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        StringBuilder sb = new StringBuilder(length);
+        for (int i = 0; i < length; i++) {
+            int index = (int) (Math.random() * characters.length());
+            sb.append(characters.charAt(index));
+        }
+        return sb.toString();
+    }
+
+    public static String generateRandomEmail() {
+        return generateRandomString(8) + "@" + generateRandomString(5) + ".com";
+    }
+
+    public static String generateRandomString(int length) {
+        String characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+        StringBuilder sb = new StringBuilder(length);
+        for (int i = 0; i < length; i++) {
+            int index = (int) (Math.random() * characters.length());
+            sb.append(characters.charAt(index));
+        }
+        return sb.toString();
+    }
+
+    public static String generateRandomPhoneNumber() {
+        return "+1" + (int) (Math.random() * 1000000000);
+    }
+
+}

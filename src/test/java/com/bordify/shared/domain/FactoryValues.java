b3e2cdf496c0c2b42ba9f5d12a74ca0bdb2ab8c9
JuanC <abjuancarlos.12@gmail.com>
2024-09-17 21:20:33 -0500

    feat: refactoring class and implement method to generate values random

--- src/test/java/com/bordify/shared/domain/FactoryValues.java
+++ src/test/java/com/bordify/shared/domain/FactoryValues.java
@@ -1,44 +1,47 @@
 package com.bordify.shared.domain;
 
+import static com.bordify.utils.GeneratorValuesRandom.generateRandomValue;
+
 public class FactoryValues {
 
     public static String generateRandomAlphanumeric(int length) {
+
         String characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
         StringBuilder sb = new StringBuilder(length);
-        for (int i = 0; i < length; i++) {
-            int index = (int) (Math.random() * characters.length());
-            sb.append(characters.charAt(index));
-        }
-        return sb.toString();
+        return generateRandomizeString(length, characters);
     }
 
     public static String generateRandomAlphanumeric(int start, int end) {
-        int length = (int)(Math.random() * (end - start) + start);
+
+        int length = generateRandomValue(start, end);
         String characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-        StringBuilder sb = new StringBuilder(length);
-        for (int i = 0; i < length; i++) {
-            int index = (int) (Math.random() * characters.length());
-            sb.append(characters.charAt(index));
-        }
-        return sb.toString();
+        return generateRandomizeString(length, characters);
     }
 
     public static String generateRandomEmail() {
-        return generateRandomString(8) + "@" + generateRandomString(5) + ".com";
+
+        int usernameEmailLenght = generateRandomValue(5,10);
+        int domainEmailLenght = generateRandomValue(2,5);
+        return generateRandomString(usernameEmailLenght) + "@" + generateRandomString(domainEmailLenght) + ".com";
     }
 
     public static String generateRandomString(int length) {
+
         String characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-        StringBuilder sb = new StringBuilder(length);
-        for (int i = 0; i < length; i++) {
-            int index = (int) (Math.random() * characters.length());
-            sb.append(characters.charAt(index));
-        }
-        return sb.toString();
+        return generateRandomizeString(length, characters);
     }
 
     public static String generateRandomPhoneNumber() {
-        return "+1" + (int) (Math.random() * 1000000000);
+        return "+1" + generateRandomValue(1000000000);
+    }
+
+    public static String generateRandomizeString(int length, String baseString){
+        StringBuilder sb = new StringBuilder(length);
+        for (int i = 0; i < length; i++) {
+            int index = generateRandomValue(length);
+            sb.append(baseString.charAt(index));
+        }
+        return sb.toString();
     }
 
 }
